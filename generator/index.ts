010a82e31894bb1b2d04325b1ffa3b1f89ed3dba
Kirill Petkov <sardonyx.wt@gmail.com>
2018-05-16 17:47:13 +0300

    increment version 1.6.3 => 1.6.4 fix generator util uniqueId function

--- generator/index.ts
+++ generator/index.ts
@@ -12,7 +12,7 @@ export function generateSeed(length: number) {
 }
 
 export function uniqueId(prefix = '', useSeed = false) {
-  indexes[prefix] ? ++indexes[prefix] : indexes[prefix] = 0;
+  indexes[prefix] ? ++indexes[prefix] : indexes[prefix] = 1;
   let result = prefix + indexes[prefix];
   if (useSeed) result = `${result}-${generateSeed(8).join('')}`;
   return result;
